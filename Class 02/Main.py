8172cde8f29db8dc09d5f17a0eeb5b477fb4d761
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-08-09 22:19:54 -0400

    Added Sigmoid neuron implementation and graphical visualization.

--- Class 02/Main.py
+++ Class 02/Main.py
@@ -1,58 +1,112 @@
 import random
-from perceptron.Perceptron import Perceptron
+from perceptron.Neurons import Perceptron, Sigmoid
 import matplotlib.pyplot as plt
 
-def f(anX):
-    return anX*4 + 1
+
+def f(an_x):
+    return an_x*4 + 1
 
 
-canvas = 50
-training = 100
-points = 500
+def make_visual_graph(a_neuron, canvas, training_iterations, showing_points, threshold=1):
+
+    x_function = list()
+    y_function = list()
+
+    x_red_point = list()
+    y_red_point = list()
+    x_blue_point = list()
+    y_blue_point = list()
+
+    for _ in range(training_iterations):
+        rand_x = random.uniform(-canvas, canvas)
+        rand_y = random.uniform(-canvas, canvas)
+
+        ideal_output = 0
+        if f(rand_x) < rand_y:
+            ideal_output = 1
+
+        a_neuron.train([rand_x, rand_y], ideal_output)
+
+    for i in range(0, showing_points):
+        x = random.uniform(-canvas, canvas)
+        y = random.uniform(-canvas, canvas)
+
+        output = a_neuron.evaluate([x, y])
+
+        if output >= threshold:
+            x_red_point.append(x)
+            y_red_point.append(y)
+        else:
+            x_blue_point.append(x)
+            y_blue_point.append(y)
+
+    for j in range(2*int(-canvas/3), 2*int(canvas/3)):
+        x_function.append(j)
+        y_function.append(f(j))
+
+    plt.plot(x_red_point, y_red_point, 'ro', x_blue_point, y_blue_point, 'bo', x_function, y_function, 'g')
+    plt.show()
 
 
-xx = list()
-yy = list()
+def make_learning_graph(canvas, iterations=200, calc_points=100, threshold=1.):
 
-rx = list()
-ry = list()
-bx = list()
-by = list()
+    perceptron_training_correctness = list()
+    sigmoid_training_correctness = list()
 
-per = Perceptron([1., 5.], -2.)
+    for iteration in range(iterations):
+        perceptron = Perceptron([1., 5.], -1)
+        sigmoid = Sigmoid([1., 5.], -1, 0.5)
 
-print(per.weights)
+        # training
+        for train in range(iteration):
+            rand_x = random.uniform(-canvas, canvas)
+            rand_y = random.uniform(-canvas, canvas)
 
-for j in range(0,training):
-    x1 = random.uniform(-canvas, canvas)
-    y1 = random.uniform(-canvas, canvas)
+            ideal_output = 0
+            if f(rand_x) < rand_y:
+                ideal_output = 1
 
-    if f(x1) < y1:
-        expected = 1
-    else:
-        expected = 0
+            perceptron.train([rand_x, rand_y], ideal_output)
+            sigmoid.train([rand_x, rand_y], ideal_output)
 
-    res = per.train([x1, y1], expected)
+        #evaluation
+        correct_points_perceptron = 0
+        correct_points_sigmoid = 0
+        for point in range(calc_points):
+            rand_x = random.uniform(-canvas, canvas)
+            rand_y = random.uniform(-canvas, canvas)
 
-print(per.weights)
+            ideal_output = 0
+            if f(rand_x) < rand_y:
+                ideal_output = 1
 
-for i in range(0,points):
-    x = random.uniform(-canvas, canvas)
-    y = random.uniform(-canvas, canvas)
+            perceptron_output = perceptron.evaluate([rand_x, rand_y])
+            sigmoid_output = sigmoid.evaluate([rand_x, rand_y])
+            normalized_output = 1
+            if sigmoid_output < threshold:
+                normalized_output = 0
 
-    res = per.evaluate([x, y])
+            if ideal_output == normalized_output:
+                correct_points_sigmoid += 1
+            if ideal_output == perceptron_output:
+                correct_points_perceptron += 1
 
-    if res:
-        rx.append(x)
-        ry.append(y)
-    else:
-        bx.append(x)
-        by.append(y)
+        perceptron_training_correctness.append(correct_points_perceptron / calc_points)
+        sigmoid_training_correctness.append(correct_points_sigmoid / calc_points)
 
-for j in range(int(-canvas/2), int(canvas/2)):
-    xx.append(j)
-    yy.append(f(j))
+    x_axis = list()
+    for i in range(iterations):
+        x_axis.append(i)
+
+    plt.plot(x_axis, perceptron_training_correctness, 'b', x_axis, sigmoid_training_correctness, 'r', alpha=0.5)
+    axes = plt.gca()
+    axes.set_ylim([0, 1])
+    plt.show()
 
 
-plt.plot(rx, ry, 'ro', bx, by, 'bo', xx, yy, 'g')
-plt.show()
+# good curve
+make_learning_graph(50, iterations=1000, threshold=1.)
+# no
+make_learning_graph(50, iterations=200, threshold=0.5)
+# no
+make_learning_graph(50, iterations=200, threshold=0.2)
---
+++ Class 02/perceptron/Neurons.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+learning_rate = 0.01
+
+
+class Perceptron:
+
+    def __init__(self, weight_list, bias):
+        self.weights = np.array(weight_list)
+        self.bias = bias
+
+    def evaluate(self, input_list):
+        try:
+            assert len(self.weights) == len(input_list)
+
+            res_sum = sum(self.weights * np.array(input_list))
+            res = 0
+            if res_sum + self.bias > 0:
+                res = 1
+            return res
+
+        except AssertionError:
+            print("The length of the input defers from the weights vector.")
+        except TypeError:
+            print("You either entered a non number list in constructor or input. Types don't match.")
+
+    def train(self, input_train_list, expected_result):
+        local_result = self.evaluate(input_train_list)
+        if local_result > expected_result:
+            self.decrease_weights(input_train_list)
+        elif local_result < expected_result:
+            self.increase_weights(input_train_list)
+
+    def decrease_weights(self, inputs):
+        self.weights -= np.array(inputs) * learning_rate
+
+    def increase_weights(self, inputs):
+        self.weights += np.array(inputs) * learning_rate
+
+
+class Sigmoid(Perceptron):
+
+    def __init__(self, weight_list, bias, threshold=0.5):
+        super().__init__(weight_list, bias)
+        self.threshold = threshold
+
+    def evaluate(self, input_list):
+        try:
+            assert len(self.weights) == len(input_list)
+
+            res_sum = sum(self.weights * np.array(input_list))
+            return self.activation_function(res_sum)
+
+        except AssertionError:
+            print("The length of the input defers from the weights vector.")
+        except TypeError:
+            print("You either entered a non number list in constructor or input. Types don't match.")
+
+    def train(self, input_train_list, expected_result):
+        local_result = self.evaluate(input_train_list)
+
+        normalized_output = 1
+        if local_result < self.threshold:
+            normalized_output = 0
+
+        if normalized_output > expected_result:
+            self.decrease_weights(input_train_list)
+        elif normalized_output < expected_result:
+            self.increase_weights(input_train_list)
+
+    @staticmethod
+    def activation_function(z):
+        value = np.exp(-z)
+        return 1 / (1 + value)

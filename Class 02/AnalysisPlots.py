b23820840b47aeee4bc433dd12142f1f0fb4537a
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-09-03 16:06:21 -0300

    Fix bug in analysis network training and refactoring

--- Class 02/AnalysisPlots.py
+++ Class 02/AnalysisPlots.py
@@ -1,66 +1,24 @@
 import csv
 import random
-import pandas as pd
-from Neurons.NeuralNetwork import NeuralNetwork
-import matplotlib.pyplot as plt
+
 import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from Neurons.NeuralNetwork import NeuralNetwork
+from network_utilities import generate_network
+
 matplotlib.style.use('ggplot')
 
 
-def get_rand():
-    return random.random()
 
-
-def prettify_network(a_network):
-    for layer in a_network:
-        for neuron in layer:
-            print(neuron)
-        print("")
-
-
-# generates a random network with the input parameters
-def generate_network(n_inputs, neurons_output, neurons_per_layer, layers):
-    a_network = list()
-
-    first_layer = list()
-    # input
-    # input is number of weights in initial layer
-    for input_n in range(neurons_per_layer):
-        input_neuron = list()
-        for n_input in range(n_inputs + 1):
-            input_neuron.append(get_rand())
-        first_layer.append(input_neuron)
-    a_network.append(first_layer)
-
-    # rest
-    for layer in range(layers - 2):
-        a_layer = list()
-        for neuron in range(neurons_per_layer):
-            a_neuron = list()
-            for _ in range(neurons_per_layer + 1):
-                a_neuron.append(get_rand())
-            a_layer.append(a_neuron)
-        a_network.append(a_layer)
-
-    last_layer = list()
-    # out
-    # out is number of neurons in outer later
-    for out_n in range(neurons_output):
-        output_neuron = list()
-        for n_out in range(neurons_per_layer + 1):
-            output_neuron.append(get_rand())
-        last_layer.append(output_neuron)
-    a_network.append(last_layer)
-
-    return a_network
 
 
 # Real test with seeds dataset
 
 # Impact of hidden layers in learning rate
-def impact_hidden_layer(train_set, test_set, train_expected, test_expected, total_layers=10, epochs=1000,
+def impact_hidden_layer(train_set, test_set, train_expected, test_expected, neuron_layer=3, total_layers=10, epochs=1000,
                         epoch_step=10, trials=100, output_file="hidden_impact.csv"):
-
     assert total_layers > 0
 
     dataset = list()
@@ -71,12 +29,15 @@ def impact_hidden_layer(train_set, test_set, train_expected, test_expected, tota
         writer = csv.writer(file)
         writer.writerow(["epoch", "layer", "correctness"])
 
-        for layer in range(2, total_layers + 1):
-            network = NeuralNetwork(manual=True)
-            network.initialize(network=generate_network(n_inputs=2, neurons_output=2,
-                                                        neurons_per_layer=3, layers=layer))
+        for layer in range(1, total_layers + 1):
+
+            a_network = generate_network(n_inputs=len(train_set[0]), neurons_output=len(train_expected[0]),
+                                         neurons_per_layer=neuron_layer, layers=layer)
 
             for epoch in range(1, epochs, epoch_step):
+                network = NeuralNetwork(manual=True)
+                network.initialize(network=a_network)
+
                 print("layer: {}\nepoch: {}\n".format(layer, epoch))
                 # training
                 network.train_with_dataset(dataset=dataset, epoch=epoch)
@@ -95,14 +56,51 @@ def impact_hidden_layer(train_set, test_set, train_expected, test_expected, tota
                 writer.writerow([epoch, layer, correctness / trials])
 
 
-# TODO
 # Impact of number of neurons in learning rate
+def impact_number_neuron(train_set, test_set, train_expected, test_expected, max_neurons_per_layer=100, layers=2,
+                         epochs=1000, epoch_step=10, trials=100, output_file="neuron_impact.csv"):
+    assert max_neurons_per_layer > 0
+
+    dataset = list()
+    for data in zip(train_set, train_expected):
+        dataset.append(list(data))
+
+    with open(output_file, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["epoch", "neuron_number", "correctness"])
+
+        for neuron in range(1, max_neurons_per_layer + 1):
+
+            a_network = generate_network(n_inputs=len(train_set[0]), neurons_output=len(train_expected[0]),
+                                         neurons_per_layer=neuron, layers=layers)
+
+            for epoch in range(1, epochs, epoch_step):
+                network = NeuralNetwork(manual=True)
+                network.initialize(network=a_network)
+
+                print("neuron: {}\nepoch: {}\n".format(neuron, epoch))
+                # training
+                network.train_with_dataset(dataset=dataset, epoch=epoch)
+
+                # evaluation
+                correctness = 0.
+                tests = list(zip(test_set, test_expected))
+                for t in range(trials):
+                    _test = random.choice(tests)
+
+                    ideal_output = _test[1]
+                    actual = network.feed(_test[0])
+                    normalized_output = [round(e, 0) for e in actual]
+                    if ideal_output == normalized_output:
+                        correctness += 1.
+                writer.writerow([epoch, neuron, correctness / trials])
+
 
 # TODO
 # How fast data is analysed
 
 # TODO
-# Effects of different learning rates
+# Effects of different learning rates7
 
 # TODO
 # Comparison with sorted vs shuffled data
@@ -110,8 +108,14 @@ def impact_hidden_layer(train_set, test_set, train_expected, test_expected, tota
 
 def plot_hidden_impact(file_location="Analysis/hidden_impact.csv"):
     data_hidden = pd.read_csv(file_location)
-    data_hidden.plot.scatter(x='epoch', y='correctness', c='layer', s=50, colormap='gist_rainbow',
-                             title="Number of hidden layers vs learning rate")
+    data_hidden.plot.scatter(x='epoch', y='layer', c='correctness', s=50, colormap='gist_rainbow',
+                             title="Number of layers vs learning rate with 3 neurons per layer")
+    plt.show()
+
+def plot_neuron_impact(file_location="Analysis/neuron_impact.csv"):
+    data_neuron = pd.read_csv(file_location)
+    data_neuron.plot.scatter(x='epoch', y='neuron_number', c='correctness', s=50, colormap='gist_rainbow',
+                             title="Number of neurons vs learning rate with 2 layers")
     plt.show()
 
 
@@ -140,8 +144,19 @@ test_expected_XOR = [
     [0, 1]
 ]
 
-hidden_file = "Analysis/hidden_impact.csv"
-# impact_hidden_layer(train_set_XOR, test_set_XOR, train_expected_XOR,
-# test_expected_XOR, epochs=1000, total_layers=10, output_file=hidden_file)
+hidden_file = "Analysis/hidden_impact2.csv"
+#neuron_file = "Analysis/neuron_impact.csv"
+#
+impact_hidden_layer(train_set_XOR, test_set_XOR, train_expected_XOR, test_expected_XOR,
+                    epochs=600, total_layers=10, neuron_layer=3, output_file=hidden_file)
+#
+# impact_number_neuron(train_set_XOR, test_set_XOR, train_expected_XOR, test_expected_XOR,
+#                      epochs=1000, max_neurons_per_layer=20, output_file=neuron_file)
+
+# data_hidden = pd.read_csv(neuron_file)
+# data_hidden.plot.scatter(x='epoch', y='correctness', c='neuron_number', s=50, colormap='gist_rainbow')
+# plt.show()
 
 plot_hidden_impact(hidden_file)
+
+#plot_neuron_impact(neuron_file)
--- Class 02/NetworkPlots.py
+++ Class 02/NetworkPlots.py
@@ -3,53 +3,60 @@ import random
 import matplotlib.pyplot as plt
 
 from Neurons.NeuralNetwork import NeuralNetwork
+from network_utilities import generate_network
 
 
-def make_learning_graph(iterations=1000, evaluation=100):
+def make_learning_graph(train_set, test_set, train_expected, test_expected, iterations=1000, evaluation=100):
+    dataset = list()
+    for data in zip(train_set, train_expected):
+        dataset.append(list(data))
+
     network_correctness = list()
     total_error = list()
-    for iteration in range(1, iterations):
+
+    a_network = generate_network(n_inputs=len(train_set[0]), neurons_output=len(train_expected[0]),
+                                 neurons_per_layer=3, layers=3)
+    for iteration in range(1, iterations, 10):
         network = NeuralNetwork(manual=True)
-        network.initialize(network=[
-            [
-                [0.52005262, 0.50090067, 0.6476664410034432],
-                [0.6634167, 0.34459219, 0.934842553959054],
-                [0.7176642, 0.41593824, 0.1908567125038647]
-            ],
-            [
-                [0.05263594, 0.63529249, 0.89631397, 0.7987314051068816]
-            ]
-        ])
+        network.initialize(network=a_network)
+        # network = NeuralNetwork(manual=True)
+        # network.initialize(network=[
+        #     [
+        #         [0.52005262, 0.50090067, 0.6476664410034432],
+        #         [0.6634167, 0.34459219, 0.934842553959054],
+        #         [0.7176642, 0.41593824, 0.1908567125038647]
+        #     ],
+        #     [
+        #         [0.52005262, 0.50090067, 0.50090067, 0.6476664410034432],
+        #         [0.6634167, 0.34459219, 0.50090067, 0.934842553959054],
+        #         [0.7176642, 0.41593824, 0.50090067, 0.1908567125038647]
+        #     ],
+        #     [
+        #         [0.05263594, 0.63529249, 0.89631397, 0.7987314051068816],
+        #         [0.52005262, 0.63529249, 0.50090067, 0.6476664410034432]
+        #     ]
+        # ])
 
         print(iteration)
         # training
-        error = network.train_with_dataset(dataset=[
-            [[0., 0.], [0]],
-            [[1., 1.], [0]],
-            [[1., 0.], [1]],
-            [[0., 1.], [1]]
-        ], epoch=iteration)
+        error = network.train_with_dataset(dataset=dataset, epoch=iteration)
 
         # evaluation
-        correctness = 0
+        correctness = 0.
+        tests = list(zip(test_set, test_expected))
         for test in range(evaluation):
-            bool1 = random.randint(0, 1)
-            bool2 = random.randint(0, 1)
-
-            ideal_output = bool1 ^ bool2
-
-            actual = network.feed([bool1, bool2])
-            normalized_output = round(actual[0], 0)
-
+            _test = random.choice(tests)
+            ideal_output = _test[1]
+            actual = network.feed(_test[0])
+            normalized_output = [round(e, 0) for e in actual]
             if ideal_output == normalized_output:
-                correctness += 1
-
+                correctness += 1.
         if error:
             total_error.append(error[-1])
         network_correctness.append(correctness / evaluation)
 
     x_axis = list()
-    for i in range(1, iterations):
+    for i in range(1, iterations, 10):
         x_axis.append(i)
 
     #total_error.insert(1, total_error[0])
@@ -64,5 +71,29 @@ def make_learning_graph(iterations=1000, evaluation=100):
     plt.title('Training of XOR')
     plt.show()
 
+train_set_XOR = [
+    [0., 0.],
+    [1., 1.],
+    [1., 0.],
+    [0., 1.]
+]
+train_expected_XOR = [
+    [1, 0],
+    [1, 0],
+    [0, 1],
+    [0, 1]
+]
+test_set_XOR = [
+    [0., 0.],
+    [1., 1.],
+    [1., 0.],
+    [0., 1.]
+]
+test_expected_XOR = [
+    [1, 0],
+    [1, 0],
+    [0, 1],
+    [0, 1]
+]
 
-make_learning_graph(iterations=100)
+make_learning_graph(train_set_XOR, test_set_XOR, train_expected_XOR, test_expected_XOR, iterations=600)
